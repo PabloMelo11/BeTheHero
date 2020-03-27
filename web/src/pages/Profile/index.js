1c8f60ae3ee94e104b518f2d902800aadfb4f4a7
Pablo Melo <pablomelodev@gmail.com>
2020-03-27 01:40:34 -0300

    Delete incidents

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -26,6 +26,20 @@ export default function Profile() {
       });
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert("Erro ao deletar caso, tente novamente");
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -58,7 +72,7 @@ export default function Profile() {
               }).format(incident.value)}
             </p>
 
-            <button>
+            <button onClick={() => handleDeleteIncident(incident.id)}>
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
