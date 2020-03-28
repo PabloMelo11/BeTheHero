a62fde79bf8fc541e1d7419794191b9cfd8d016f
Pablo Melo <pablomelodev@gmail.com>
2020-03-28 00:08:01 -0300

    Send email

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { View, Image, Text, TouchableOpacity } from "react-native";
+import * as MailComposer from "expo-mail-composer";
 
 import logoImg from "../../assets/logo.png";
 
@@ -9,10 +10,22 @@ import styles from "./styles";
 
 export default function Detail() {
   const navigation = useNavigation();
+  const message =
+    'Ola APDA, estou entrando emc contato, pois gostaria ajudar no caso "Cadelinha atropelada" com o valor de R$120';
 
   function navigateBack() {
     navigation.goBack();
   }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: "Heroi do caso: Cadelinha atropelada",
+      recipients: ["pablo@gmail.com"],
+      body: message
+    });
+  }
+
+  function sendWhatsapp() {}
 
   return (
     <View style={styles.container}>
@@ -46,7 +59,7 @@ export default function Detail() {
             <Text style={styles.actionText}>Whatsapp</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
