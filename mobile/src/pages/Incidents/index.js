8ff6be21b5632d2db112f1783043e36dfef321c7
Pablo Melo <pablomelodev@gmail.com>
2020-03-28 01:42:29 -0300

    Loading in page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,6 +12,8 @@ import styles from "./styles";
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
@@ -20,11 +22,23 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const response = await api.get("incidents");
+    if (loading) {
+      return;
+    }
 
+    if (total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get("incidents", {
+      params: { page }
+    });
     setTotal(response.headers["x-total-count"]);
-
-    setIncidents(response.data);
+    setIncidents([...incidents, ...response.data]);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -49,7 +63,8 @@ export default function Incidents() {
         style={styles.incidentList}
         data={incidents}
         keyExtractor={incident => String(incident.id)}
-        showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
